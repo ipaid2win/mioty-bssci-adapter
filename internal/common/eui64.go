350f373fe5bcb7682ce4be849c2edc0c83fea50f
doernera <alexander.t.doerner@gmail.com>
2025-02-23 22:41:34 +0100

    helpers

--- internal/common/eui64.go
+++ internal/common/eui64.go
@@ -6,12 +6,11 @@ import (
 	"strings"
 )
 
-
 // EUI64 data type
 type EUI64 [8]byte
 
 // helper function to parse a int64 formatted eui64
-func EUI64FromInt(in int64) EUI64 {
+func Eui64FromInt(in int64) EUI64 {
 	b := [8]byte{
 		byte(0xff & in),
 		byte(0xff & (in >> 8)),
@@ -24,6 +23,17 @@ func EUI64FromInt(in int64) EUI64 {
 	}
 
 	return b
+}
+
+// helper function to parse eui64 into a int64 
+func (e *EUI64) ToInt() int64 {
+	return int64(e[0]) | int64(e[1])<<8 | int64(e[2])<<16 | int64(e[3])<<24 |
+		int64(e[4])<<32 | int64(e[5])<<40 | int64(e[6])<<48 | int64(e[7])<<56
+}
+
+// helper function to parse eui64 into a int64 
+func Eui64toInt(e *EUI64) int64 {
+	return e.ToInt()
 }
 
 
@@ -39,7 +49,7 @@ func (e *EUI64) UnmarshalText(text []byte) error {
 		return err
 	}
 	if len(e) != len(b) {
-		return fmt.Errorf("lorawan: exactly %d bytes are expected", len(e))
+		return fmt.Errorf("eui64: exactly %d bytes are expected", len(e))
 	}
 	copy(e[:], b)
 	return nil
@@ -63,7 +73,7 @@ func (e EUI64) MarshalBinary() ([]byte, error) {
 // UnmarshalBinary implements encoding.BinaryUnmarshaler.
 func (e *EUI64) UnmarshalBinary(data []byte) error {
 	if len(data) != len(e) {
-		return fmt.Errorf("lorawan: %d bytes of data are expected", len(e))
+		return fmt.Errorf("eui64: %d bytes of data are expected", len(e))
 	}
 	for i, v := range data {
 		// little endian
